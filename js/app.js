eac49dc82d001a813c5bea5323299ef793317c5f
MeteoSignal <yoann.bourillon@yahoo.fr>
2026-06-29 12:25:52 +0200

    Modif F

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 console.log(`${CONFIG.appName} v${CONFIG.version} démarré`);
 
 document.getElementById("version").textContent =
-`${CONFIG.appName} • v${CONFIG.version} • Build ${CONFIG.build} • Développement actif. Merci de suivre l'évolution du projet. • ${CONFIG.copyright}`;
+`${CONFIG.appName} • v${CONFIG.version} • Build ${CONFIG.build} • Développement actiff. Merci de suivre l'évolution du projet. • ${CONFIG.copyright}`;
